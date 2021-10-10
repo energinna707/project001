2a102f69df8b54ba2114135fc4717cea49099dcf
energinna707 <92246163+energinna707@users.noreply.github.com>
2021-10-10 10:57:19 +0300

    created file

---
+++ tourizm/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.slider__box').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/arrow left.svg">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="/images/Arrow right.svg" alt="arrow-right">',
+  });
+});
